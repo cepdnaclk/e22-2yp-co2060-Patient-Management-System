5ab702f853157cfca685d726b474116892923242
Moksitha Jayanga <e22159@eng.pdn.ac.lk>
2026-02-17 13:19:58 +0530

    Create Home page

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import Signup from "./features/auth/Signup.jsx";
 import Login from "./features/auth/Login.jsx";
 import "./App.css";
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
     <>
-      <Login />
+      <Home />
+      {/* <Signup /> */}
     </>
   );
 }
