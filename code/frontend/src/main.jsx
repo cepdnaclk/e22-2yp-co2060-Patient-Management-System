2a611d391918433450219d66e79c200bc50cf85c
Lahiru Eranda <e22364@eng.pdn.ac.lk>
2026-02-19 21:08:40 +0530

    Added footer

--- code/frontend/src/main.jsx
+++ code/frontend/src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/footer.jsx';
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+  
     <BrowserRouter>
       <Navbar />
       <App />
+      <Footer />
+
     </BrowserRouter>
   </StrictMode>,
 );
